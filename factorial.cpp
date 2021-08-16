59c88d5da43168b476cb1a2807dd7216d7a8cf69
Anmolpreet Singh <anmol323c@gmail.com>
2021-08-16 15:46:19 +0530

    changed factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,9 @@
 #include "functions.h"
 
 int factorial(int n){
-    if(n!=1){
+    if(n==1)return 1;
+    if(n>0){
 	return(n * factorial(n-1));
     }
-    else return 1;
+    else{cout<<"please enter a positive number"; return 0; }
 }
