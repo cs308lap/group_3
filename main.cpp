f6cb41a43081fab50a3b631ca051a2324260dbe1
aryangoyla1812 <b19073@students.iitmandi.ac.in>
2021-08-30 11:22:45 +0530

    main function modified

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ int main(){
     print_hello();
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    int x,y;
+    x = 56;
+    y = 98;
+    cout << hcf(x,y) << "\n";
     return 0;
 }
