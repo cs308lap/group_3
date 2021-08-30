d2e18b0b177e4e845f12f16d2ac85f24593cb5c9
aryangoyla1812 <b19073@students.iitmandi.ac.in>
2021-08-30 11:13:56 +0530

    iterative gcd added

---
+++ gcd.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int hcf(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+    while (a != b) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+ 
